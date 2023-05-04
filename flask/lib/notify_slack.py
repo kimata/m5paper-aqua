3b47a9965f20565862c44ad9db23a8669c35b1c2
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-05-04 20:03:52 +0900

    fix: コードを整理

--- flask/lib/notify_slack.py
+++ flask/lib/notify_slack.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
+import slack_sdk
 
 import json
 import logging
@@ -14,7 +13,7 @@ ERROR_NOTIFY_FOOTPRINT = (
     pathlib.Path(os.path.dirname(__file__)).parent / "data" / "error_notify"
 )
 
-ERROR_TMPL = """\
+SIMPLE_TMPL = """\
 [
     {{
         "type": "header",
@@ -35,21 +34,41 @@ ERROR_TMPL = """\
 """
 
 
-def send(token, channel, message, title):
-    client = WebClient(token=token)
+def format_simple(title, message):
+    return {
+        "text": message,
+        "json": json.loads(
+            SIMPLE_TMPL.format(title=title, message=json.dumps(message))
+        ),
+    }
 
-    json_message = ERROR_TMPL.format(title=title, message=json.dumps(message))
+
+def send(token, channel, title, message):
+    client = slack_sdk.WebClient(token=token)
+
     try:
         client.chat_postMessage(
             channel=channel,
-            text=message,
-            blocks=json.loads(json_message),
+            text=message["text"],
+            blocks=message["json"],
         )
-    except SlackApiError as e:
+    except slack_sdk.errors.SlackApiError as e:
         logging.warning(e.response["error"])
 
 
-def error(token, channel, message, title="エラー", interval_min=10):
+def info(token, channel, message, formatter=format_simple):
+    title = "Info"
+    send(
+        token,
+        channel,
+        title,
+        formatter(title, message),
+    )
+
+
+def error(token, channel, message, interval_min=10, formatter=format_simple):
+    title = "Error"
+
     if (
         ERROR_NOTIFY_FOOTPRINT.exists()
         and (
@@ -61,23 +80,41 @@ def error(token, channel, message, title="エラー", interval_min=10):
         logging.info("skip slack nofity")
         return
 
-    send(token, channel, message, title)
+    send(
+        token,
+        channel,
+        title,
+        formatter(title, message),
+    )
+
     ERROR_NOTIFY_FOOTPRINT.parent.mkdir(parents=True, exist_ok=True)
     ERROR_NOTIFY_FOOTPRINT.touch()
 
 
 if __name__ == "__main__":
     import logger
+    import sys
     from config import load_config
 
     logger.init("test", level=logging.WARNING)
     logging.info("Test")
 
     config = load_config()
-    error(
-        config["SLACK"]["BOT_TOKEN"],
-        config["SLACK"]["ERROR"]["CHANNEL"],
-        "メッセージ",
-        "タイトル",
-        config["SLACK"]["ERROR"]["INTERVAL_MIN"],
-    )
+    if "SLACK" not in config:
+        logging.warning("Slack の設定が記載されていません．")
+        sys.exit(-1)
+
+    if "INFO" in config["SLACK"]:
+        info(
+            config["SLACK"]["BOT_TOKEN"],
+            config["SLACK"]["INFO"]["CHANNEL"],
+            "メッセージ\nメッセージ",
+        )
+
+    if "ERROR" in config["SLACK"]:
+        error(
+            config["SLACK"]["BOT_TOKEN"],
+            config["SLACK"]["ERROR"]["CHANNEL"],
+            "エラーメッセージ",
+            config["SLACK"]["ERROR"]["INTERVAL_MIN"],
+        )
